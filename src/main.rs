a75bbf20352c4a978ce4b3c95d1372aa52012791
Xander Pickering <94504311+xandwr@users.noreply.github.com>
2026-09-08 13:38:25 -0600

    Clean restart against modern standards

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use anyhow::Result;
-use clap::Parser;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "mcpd=info".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr))
-        .init();
-
-    mcpd::cli::Cli::parse().run().await
+fn main() {
+    println!("Hello, world!");
 }
